362e09eddc5a87ce05e92f6a97afbd0b1f17f917
CYB3RL1F3 <cyberlife.music@gmail.com>
2020-06-21 18:31:16 +0200

    remove debug from service_worker

--- src/assets/pwa/service_worker.js
+++ src/assets/pwa/service_worker.js
@@ -1,6 +1,6 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
-workbox.setConfig({ debug: true });
+workbox.setConfig({ debug: false });
 workbox.core.skipWaiting();
 workbox.core.clientsClaim();
 workbox.precaching.cleanupOutdatedCaches();
